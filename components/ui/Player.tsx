cdb0fe384a4405d5278200d75d541352c08c08ca
L0RkZ <voxexxd@gmail.com>
2025-12-21 04:38:58 -0600

    feat: implement fullscreen functionality in Player component and remove expo-av dependency

--- components/ui/Player.tsx
+++ components/ui/Player.tsx
@@ -1,9 +1,18 @@
 import { useAudio } from "@/contexts/PlayerContext";
 import { MaterialIcons } from '@expo/vector-icons';
 import Slider from '@react-native-community/slider';
+import * as PortalPrimitive from '@rn-primitives/portal';
 import React, { useEffect, useRef, useState } from "react";
 import { Dimensions, Image, Modal, Pressable, Text, TouchableOpacity, useWindowDimensions, View } from "react-native";
+import Animated, {
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import VerticalSlider from "../VerticalSlider";
+
 export default function Player() {
   const { 
     currentSong,
@@ -17,6 +26,7 @@ export default function Player() {
     isLiked,
     prev,
     next,
+    PlayerHeight
   } = useAudio();
   let [newThumbnail, setnewThumbnail] = useState<string | null>(null);
   let [UpdateCurrentSong, setUpdateCurrentSong] = useState<string|null>(null);
@@ -27,16 +37,34 @@ export default function Player() {
   const volumeRef = useRef<View>(null);
   const [volumePos, setVolumePos] = useState<{ x: number; y: number; w: number; h: number } | null>(null);
   const { width, height } = useWindowDimensions();
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const insets = useSafeAreaInsets();
+  const fullScreenY = useSharedValue(1000);
+  const [shouldRender, setShouldRender] = useState(false);
+  const OFFSCREEN_Y = height + 100;
 
+  const fullScreenStyle = useAnimatedStyle(() => ({
+    transform: [{ translateY: fullScreenY.value }],
+  }));
 
   // Usamos useEffect para que no nos spamee el thumbnail, 
   // Nos aseguramos de que solo se imprima cuando realmente cambie el thumbnail
   // Esto pasa porque en nuestro context el status se va actualizando cada segundo
   useEffect(() => {
+    if (isFullScreen) {
+      setShouldRender(true);
+      fullScreenY.value = withTiming(0, { duration: 300 });
+    } else {
+      fullScreenY.value = withTiming(OFFSCREEN_Y, { duration: 200 }, (finished) => {
+        if (finished) {
+          runOnJS(setShouldRender)(false);
+        }
+      });
+    }
     modalVolumeVisble && measureVolumeBtn();
     setnewThumbnail(Thumbnail);
     setUpdateCurrentSong(currentSong);
-  }, [Thumbnail, currentSong, width, height, player.volume, Volume]);
+  }, [Thumbnail, currentSong, width, height, player.volume, Volume, isFullScreen]);
   // Protegimos que player exista
   if (!player) return null;
   // Formatear tiempo mm:ss
@@ -75,7 +103,62 @@ export default function Player() {
         const { height } = event.nativeEvent.layout;
         setPlayerHeight(height);
       }}
-      style={{display: currentSong ? "flex" : "none", backgroundColor: "#121212", borderTopWidth: 2, borderTopColor: "#333"}}>
+      style={{display: currentSong ? "flex" : "none", backgroundColor: "#121212", borderTopWidth: 2, borderTopColor: "#333", flex: 1}}>
+      {/* FullScreen */}
+      {shouldRender && (
+      <PortalPrimitive.Portal name="root">
+        <Animated.View
+          pointerEvents={isFullScreen ? "auto" : "none"}
+          style={[
+            {
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: PlayerHeight + insets.bottom + 49 + insets.top,
+              backgroundColor: "black",
+              zIndex: 50,
+            }, fullScreenStyle
+          ]}
+        >
+          {/* Boton cerrar */}
+          <TouchableOpacity
+            onPress={() => setIsFullScreen(false)}
+            style={{
+              position: "absolute",
+              top: 25,
+              alignSelf: "center",
+              zIndex: 10,
+              padding: 20,
+            }}
+          >
+            <MaterialIcons
+              name="keyboard-arrow-down"
+              size={40}
+              color="#dfdfdfff"
+            />
+          </TouchableOpacity>
+
+          {/* Thumbnail grande */}
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              resizeMode="contain"
+              source={newThumbnail ? { uri: newThumbnail } : undefined}
+              style={{
+                flex: 1,
+                width: "100%",
+              }}
+            />
+          </View>
+        </Animated.View>
+      </PortalPrimitive.Portal>
+      )}
       {/* Header Current song */}
       <View style={{
         paddingVertical: 6,
@@ -101,12 +184,14 @@ export default function Player() {
       </View>
       {/* THUMBNAIL */}
       <View style={{ flexDirection: "row", alignItems: "center", paddingHorizontal: 12, paddingBottom: 5}}>
-        <View>
+        <TouchableOpacity onPress={()=>{
+          setIsFullScreen(prev => !prev) 
+        }}>
           <Image 
             source={newThumbnail ? { uri: newThumbnail } : undefined}
             style={{ width: 60, height: 60, borderRadius: 3}} 
           />
-        </View>
+        </TouchableOpacity>
         {/* Segundos*/}
         <View style={{marginLeft: 6}}>
           <Text style={{ color: "#8f8f8fff", paddingRight: 10}}>{formatTime(status.currentTime)}</Text>
